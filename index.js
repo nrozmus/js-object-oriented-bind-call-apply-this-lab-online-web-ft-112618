8a63e000f0ab974b42a72de7986dce8ff7c8185c
Nicholas Rozmus <rozmusnicholas@gmail.com>
2019-04-03 21:43:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your code here
+function justInvoke(fn) {
+  return fn()
+}
+
+function setThisWithCall(fn, thisValue, arg) {
+  return fn.call(thisValue, arg)
+}
+
+function setThisWithApply(fn, thisValue, ourArguments) {
+  return fn.apply(thisValue, ourArguments)
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  return functionToBeCopied.bind(thisValue)
+}
